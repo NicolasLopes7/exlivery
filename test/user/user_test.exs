b98651bd5f2fbe31927b5986dab9a4f9624d334b
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2021-08-29 18:23:48 -0300

    Creating user test

---
+++ test/user/user_test.exs
@@ -0,0 +1,28 @@
+defmodule Exlivery.Users.UserTest do
+  alias Exlivery.Users.User
+  use ExUnit.Case
+
+  describe "build/5" do
+    test "should return an user struct with all valid params" do
+      {:ok, response} =
+        User.build("Nicolas", "nicolaslopesaquino@gmail.com", "11029314462", 18, "Minha rua")
+
+      expected_response = %User{
+        name: "Nicolas",
+        email: "nicolaslopesaquino@gmail.com",
+        cpf: "11029314462",
+        age: 18,
+        address: "Minha rua"
+      }
+
+      assert response == expected_response
+    end
+
+    test "should return an error, because the age are lower than 18" do
+      {result, _reason} =
+        User.build("Nicolas", "nicolaslopesaquino@gmail.com", "11029314462", 17, "Minha rua")
+
+      assert result == :error
+    end
+  end
+end
